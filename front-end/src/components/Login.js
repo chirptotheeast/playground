4bfc8b7dc10e3e091b7a0218129ace47402dfc54
chirptotheeast <alyssa.easterly@yahoo.com>
2021-02-11 12:19:07 -0500

    updated music player buttons

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Redirect } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 class Login extends Component {
   state = {
