77e18a731b0f34be24bd467c8d23ad50e161c8c6
chirptotheeast <git@github.com>
2021-05-29 14:31:02 -0400

    added proc files

---
+++ front-end/src/containers/BubbleBounce.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BubbleBounce extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
